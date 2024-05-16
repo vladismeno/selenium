b4bcb0134f63c0760f6f1eb137e1565393e5998c
vladismeno <vladismeno@gmail.com>
2024-05-15 23:13:58 -0700

    Lesson 7

--- lesson7/simple_selenium/main.py
+++ lesson7/simple_selenium/main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
